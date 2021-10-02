e1a7e33a2e95f930cf975783d18abd8b16675747
lxs7499 <59435336+frrrrrits@users.noreply.github.com>
2021-10-02 12:55:31 +0700

    add baseurl

--- plugin/Neonime.lua
+++ plugin/Neonime.lua
@@ -1,28 +1,31 @@
-plugin = {}
+plugin={}
+local url_base="https://neonime.cc"
 
-local function getinfo(url)
-  url = "https://doujindesu.id"
-  dirurl = url .. "/komik-list/page/".. current_page .."/?title=&order=update&status=&type="
-  Http.get(dirurl,function(code,content)
+local function getbaseurl()
+   return url_base
+end
+
+local function getinfo()
+  dir_url = ("%s/episode/page/%s"):format(getbaseurl(),current_page)
+  Http.get(dir_url,function(code,content)
     if (code == 200 and content) then
-      parse = jsoup.parse(content)
-      asbtable = jsoup.astable(parse,'animepost')
-      swipeRefresh.setRefreshing(false)  
+      classbb='//td[@class="bb"]/'
+      jxdocument = JXDocument(content) -- xpath
+      swipeRefresh.setRefreshing(false)
       activity.runOnUiThread(Runnable{
-        run=function()
-          for c,response in pairs(asbtable) do
-            byfragment=jsoup.byfragment(response)
-            title=jsoup.bytag(byfragment, "img").attr("title")
-            if title==nil or title=="" then title="tidak di ketahui" return end
-            url=jsoup.bytag(byfragment, "a").attr("href")
-            if url==nil or url=="" then url="error" return end
-            status=string.match(tostring(byfragment),'<div class="type">(.-)</div>'):gsub("%s","")
-            coverurl=jsoup.bytag(byfragment, "img").attr("src")
-            table.insert(data,{icon=coverurl,title=title,status=status})
+        run=function()          
+          title = jxdocument.sel(classbb..'a/@href//text()')
+          url = jxdocument.sel(classbb..'a/@href')
+          episode = jxdocument.sel(classbb..'/span//text()]')
+          coverurl = jxdocument.sel(classbb..'/img//@data-src')
+          for i=0,#title-1 do
+            if url=="" then url=nil end
+            if title=="" then title="err" end
+            table.insert(data,{url=url[i],icon=coverurl[i],title=title[i],episode=episode[i]})
           end
         end
       })
-      current_page=current_page+1
+      current_page=current_page+1-- ke page 2 dan seterusnya
       adapter.notifyDataSetChanged()
      else
       MyToast.showNetErrorToast(code,mainLay)
@@ -31,4 +34,6 @@ local function getinfo(url)
 end
 
 plugin.getinfo = getinfo
+plugin.getbaseurl = getbaseurl
+
 return plugin

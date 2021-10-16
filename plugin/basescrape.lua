946c7a434d6d797e24ef6dc4d91abd3f3a1ac5cd
lxs7499 <59435336+frrrrrits@users.noreply.github.com>
2021-10-16 19:49:37 +0700

    Update basescrape.lua

--- plugin/basescrape.lua
+++ plugin/basescrape.lua
@@ -1,10 +1,10 @@
 --
 -- Create by: frrrrrits
--- Date: 2021/10/02
+-- Date: 2021/10/16
 --
 
 plugin={}
-local url_base="url here"
+local url_base="url"
 
 local function getbaseurl()
   return url_base
@@ -12,31 +12,16 @@ end
 
 local function getinfo()
   --- %s untuk string
-  dir_url = ("%s/path url/%s"):format(getbaseurl(),current_page)
-  Http.get(dir_url,function(code,content)
-    if (code == 200 and content) then
-      swipeRefresh.setRefreshing(false)
-      activity.runOnUiThread(Runnable{
-        run=function()
-          title = "title string here"
-          url = "url content here"
-          episode = "episodd string here"
-          coverurl = "image url here"
-          -- loop method
-          -- for i=0, #title-1
-            -- send data
-            table.insert(data,{
-              url=url[i],icon=coverurl[i],
-              title=title[i],episode=episode[i]
-            })
-          -- end
-        end
-      })
-      current_page=current_page+1-- ke page 2 & seterusnya
-      adapter.notifyDataSetChanged()
-     else
-      MyToast.showNetErrorToast(code,mainLay)
-    end
+  path_url = ("%s/page path url/%s"):format(getbaseurl(),current_page)  
+  -- @httpGetInfo: hanya untuk getinfo
+  httpGetInfo(path_url,function()
+    -- gunakan metode @loop
+    -- tambahkan data
+    addGetInfoData(
+       url,title,
+       cover,episode
+    )
+    -- @end
   end)
 end
 

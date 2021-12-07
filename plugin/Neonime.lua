cf424499de1632cf5be5e357748b174aafff8162
lxs7499 <59435336+frrrrrits@users.noreply.github.com>
2021-12-07 16:42:19 +0700

    Update plugin
    updated entire code

--- plugin/Neonime.lua
+++ plugin/Neonime.lua
@@ -1,27 +1,65 @@
-plugin={}
-local url_base="https://neonime.cc"
+-- created by frrrrrits
+-- on 2021/12/7
 
-local function getbaseurl()
-  return url_base
+baseUrl = 'https://neonime.cc'
+
+latestUpdateUrl = '%s/episode/page/%s'
+latestSelector = 'div[id=episodes] > table > tbody td.bb'
+episodeSelector = 'ul.episodios> > li'
+downloadSelector = 'div.linkstv > div.sbox li'
+
+local genreSelector = 'div.metadatac b:contains(Genre) + span > a'
+local detailUrlSelector = 'div.metadatac b:contains(Serie) + span > a'
+local hasload = false
+
+-- ambil url ke detail info animenya
+function detailUrl(document)
+  return document.select(detailUrlSelector).attr('href')
 end
 
-local function getinfo()
-  --- %s untuk string
-  httpGetInfo("%s/episode/page/%s",getbaseurl(),function()
-    classbb = '//td[@class="bb"]/'
+function fetchLatestUpdate(document)
+  local aElement=document.select("a")
+  url=aElement.attr("href")
+  title=aElement.text()
+  episode=document.select("span").text()
+  thumbnail=document.select("img").attr("data-src")
+end
 
-    title = xpathparse.sel(classbb..'a/@href//text()')
-    url = xpathparse.sel(classbb..'a/@href')
-    episode = xpathparse.sel(classbb..'/span//text()]')
-    cover = xpathparse.sel(classbb..'/img//@data-src')
-
-    for i=0,#title-1 do
-      addGetInfoData(url[i],title[i],cover[i],episode[i])
+function fetchDetail(info,genre,document)
+  local genreElement=document.select(genreSelector).iterator()
+  info.title=document.select("div.cover > h1").text():gsub("Subtitle Indonesia","")
+  info.airdate=document.select("div.metadatac b:contains(Release Season) + span > a").text()
+  info.status=document.select("div.metadatac b:contains(TV Status) + span").text()
+  info.cover=document.select("div.imagen > img").attr("data-src")
+  info.description=document.select("div.contenidotv > div > p").text()
+  if !hasload then
+    -- biar ga nambah genre nya ketika di refresh
+    while genreElement.hasNext() do
+      local element=genreElement.next()
+      genre.url=element.select("a").attr("href")
+      genre.title=element.select("a").text()
+      updateGenre()
     end
-  end)
+    hasload=true
+  end
+  updateDetail()
 end
 
-plugin.getinfo = getinfo
-plugin.getbaseurl = getbaseurl
+function fetchEpisodeList(document)
+  local urlElement=document.select('div.episodiotitle > a')
+  url=urlElement.attr('href')
+  title=urlElement.text():gsub("Subtitle Indonesia","")
+  date=document.select('div.episodiotitle > span.date').text()
+end
 
-return plugin
+function fetchDownload(data,document)
+  local liElement=document.select("li")
+  if tostring(liElement):match("<li>MP4</li>") then
+   elseif tostring(liElement):match("<li>MKV</li>") then
+   else local labelElement=liElement.select("label")
+    -- url elements harus utuh a nya ex:(<a href=url>teks</a>)
+    local url=document.select("a")
+    local title=labelElement.text()
+    updateDownload(data,url,title)
+   end
+end
